3491a66f7e2538ad434ce65c9dab8d21ab60b064
nithyananda <nithyananda166@gmail.com>
2025-05-29 15:46:47 -0500

    Fix ESLint warnings and cleanup unused imports

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,11 +13,7 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button
-          variant='primary'
-          href={props.ghLink}
-          target='https://github.com/NithyanandaReddyMarri?tab=repositories'
-        >
+        <Button variant='primary' href={props.ghLink} target='_blank'>
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
